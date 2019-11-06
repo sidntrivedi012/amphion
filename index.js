e9e58d0fb8f545fa5ba45ec050b728e04dada029
sidntrivedi012 <sidntrivedi012@gmail.com>
2019-11-07 03:22:29 +0530

    Added http-server and basic code

---
+++ index.js
@@ -0,0 +1,28 @@
+const uploadedimage = document.getElementById("imageUpload");
+console.log(faceapi.nets);
+
+Promise.all([
+  faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
+  faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
+  faceapi.nets.ageGenderNet.loadFromUri("/models")
+]).then(hello);
+
+async function hello() {
+  document.body.append("Hello");
+  uploadedimage.addEventListener("change", async () => {
+    const image = await faceapi.bufferToImage(uploadedimage.files[0]);
+
+    const result = await faceapi
+      .detectAllFaces(image)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+
+    document.body.append(result.length);
+  });
+
+  //   const displaySize = { width: input.width, height: input.height };
+  //   // resize the overlay canvas to the input dimensions
+  //   const canvas = document.getElementById("overlay");
+  //   faceapi.matchDimensions(canvas, displaySize);
+}
