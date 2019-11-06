6701a052ce422181a0d85a960b6b2f6c35bd8607
sidntrivedi012 <sidntrivedi012@gmail.com>
2019-11-07 04:11:51 +0530

    extracting data for one face done

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
-  faceapi.nets.ageGenderNet.loadFromUri("/models")
+  faceapi.nets.ageGenderNet.loadFromUri("/models"),
+  faceapi.nets.faceExpressionNet.loadFromUri("/models")
 ]).then(hello);
 
 async function hello() {
@@ -16,8 +17,23 @@ async function hello() {
     const result = await faceapi
       .detectAllFaces(image)
       .withFaceLandmarks()
+      .withFaceExpressions()
+      .withAgeAndGender()
       .withFaceDescriptors();
+    // console.log(result[0]);
 
+    // console.log(result[0].age);
+    // console.log(result[0].gender);
+    let arr = Object.values(result[0].expressions);
+    let max = Math.max(...arr);
+    if (max == result[0].expressions.angry) express = "Angry";
+    else if (max == result[0].expressions.disgusted) express = "Disgusted";
+    else if (max == result[0].expressions.fearful) express = "Fearful";
+    else if (max == result[0].expressions.happy) express = "Happy";
+    else if (max == result[0].expressions.neutral) express = "Neutral";
+    else if (max == result[0].expressions.sad) express = "Sad";
+    else if (max == result[0].expressions.surprised) express = "Surprised";
+    // console.log(express);
     document.body.append(result.length);
   });
 }
